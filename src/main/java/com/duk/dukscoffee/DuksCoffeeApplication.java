51e8ea800fa6bb2a20eea0c3a41a553a6cc06676
angeldev07 <angelgaran20@gmail.com>
2023-11-19 18:11:24 -0500

    First Commit: Init project

---
+++ src/main/java/com/duk/dukscoffee/DuksCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.duk.dukscoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuksCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuksCoffeeApplication.class, args);
+	}
+
+}
